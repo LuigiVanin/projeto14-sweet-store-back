dbce891b5000eba1d79aa62f7f669fb968ee7c8e
LuigiVanin <luisfvaninmartins@gmail.com>
2022-05-10 23:20:35 -0300

    feat: rota de login feita

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,14 +1,24 @@
 import db from "../database.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const logIn = async (req, res) => {
-    console.log(req.body);
-
+    const { email, password } = req.body;
     try {
-        const busca = await db.collection("users").find({}).toArray();
-        console.log(busca);
-        return res.status(200).send({ message: "ok" });
-    } catch {
-        console.log("something is in the way");
+        const user = await db.collection("users").findOne({ email });
+        console.log(user);
+        if (!user) {
+            return res.status(404).send({ message: "usuário inexistente" });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.status(401).send({ message: "senha está incorreta" });
+        }
+        const payload = { email, id: user._id.toString() };
+        const token = jwt.sign(payload, process.env.JWT_KEY);
+        return res.status(200).send({ email, name: user.name, token });
+    } catch (err) {
+        console.log(err);
+        return res.sendStatus(500);
     }
 };
 
