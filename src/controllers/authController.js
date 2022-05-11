2513c35b7066f5ffdd49fd6cf941c7d502d01594
Nicole J. C. Lemes <nicolejdecarvalho@gmail.com>
2022-05-11 13:07:17 -0300

    feat: sign-up route

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 import db from "../database.js";
+import bcrypt from 'bcrypt';
 
 const logIn = async (req, res) => {
     console.log(req.body);
@@ -12,4 +13,34 @@ const logIn = async (req, res) => {
     }
 };
 
-export { logIn };
+const signUp = async (req, res) => {
+    const {name, email, password, cardNumber, cardCode, cardExpire, cardName, cardType} = req.body
+
+    try {
+        const user = await db.collection("users").findOne({email});
+
+        if (user) {
+            res.status(409).send("Usuário já existe");
+        }
+
+        await db.collection("users").insertOne({
+            name,
+            email,
+            password: bcrypt.hashSync(password, Number(process.env.KEY)),
+            cardNumber,
+            cardCode,
+            cardExpire,
+            cardName,
+            cardType
+        });
+
+        return res.status(201).send("Usuário criado com sucesso!");
+
+    } catch (error) {
+        console.log("Erro ao cadastrar usuário");
+        return res.sendStatus(500)
+    }
+};
+
+
+export { logIn, signUp };
--- src/helpers/schemas.js
+++ src/helpers/schemas.js
@@ -1,8 +1,23 @@
 import Joi from "joi";
+import dayjs from 'dayjs';
+
+const date = dayjs().format("MM/YYYY");
 
 const logInSchema = Joi.object({
     email: Joi.string().email().required(),
-    password: Joi.string().required(),
+    password: Joi.string().required(),  // vai deixar a senha sendo só string mesmo?
 });
 
-export { logInSchema };
+const signUpSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    confirmedPassword: Joi.string().ref('password').required(),
+    cardNumber: Joi.number().min(16).max(16).required(),
+    cardCode: Joi.number().min(3).max(3).required(),
+    cardExpire: Joi.date().greater(date),
+    cardName: Joi.string().required(),
+    cardType: Joi.string().valid("debit", "credit").required()
+});
+
+export { logInSchema, signUpSchema };
