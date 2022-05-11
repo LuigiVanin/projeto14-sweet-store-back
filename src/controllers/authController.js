0a3f1c106d810f93796bae4543586e5a165b4a10
LuigiVanin <luisfvaninmartins@gmail.com>
2022-05-10 22:48:40 -0300

    feat: adiçao do banco de dados

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,15 @@
-const logIn = (req, res) => {
+import db from "../database.js";
+
+const logIn = async (req, res) => {
     console.log(req.body);
-    return res.status(200).send({ message: "ok" });
+
+    try {
+        const busca = await db.collection("users").find({}).toArray();
+        console.log(busca);
+        return res.status(200).send({ message: "ok" });
+    } catch {
+        console.log("something is in the way");
+    }
 };
 
 export { logIn };
